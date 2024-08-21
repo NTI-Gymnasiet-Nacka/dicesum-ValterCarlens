45b80d2059ba25cfeae501001d11ed8fa496935d
ValterCarlens <valter.carlens@elev.ga.ntig.se>
2024-08-21 11:15:04 +0200

    probability calculator

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # Dice sum probability calculator
 # Författare: 
 # Datum:
+import os
+os.system("cls")
 
 def main():
-    user_input = input().split(" ")
+    user_input = [int(x) for x in input("").split(" ")]
+    min_dice = min(user_input)
+    max_dice = max(user_input)
+
+    for sum in range(min_dice + 1, max_dice + 2):
+        print(sum)
 
 if __name__ == "__main__":
     main()
